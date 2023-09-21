e885bf7aa3153235e18621e8bf5b1c174a4f07c5
M0nkeyPr0grammer <randoriumknights@gmail.com>
2023-09-21 11:04:09 -0500

    Add in Missing Groups

--- kubejs/client_scripts/LandscapesReimagined_ClientTweaks.js
+++ kubejs/client_scripts/LandscapesReimagined_ClientTweaks.js
@@ -137,7 +137,8 @@ REIEvents.groupEntries(event => {
         { id: "kubejs:rei_groups/regions", name: "Regions Unexplored", items: [InputItem.of('@regions_unexplored')] },
         { id: "kubejs:rei_groups/waystones", name: "Waystones", items: [InputItem.of('@waystones')] },
         { id: "kubejs:rei_groups/wunderreich", name: "Wunderreich", items: [InputItem.of('@wunderreich')] },
-        
+        { id: "kubejs:rei_groups/wilderwild", name: "Wilder Wild", items: [InputItem.of('@wilderwild')] },
+        { id: "kubejs:rei_groups/wildlife", name: "Wildlife", items: [InputItem.of('@wildlife')] },
     ];
 
     for (const group of groups) {
