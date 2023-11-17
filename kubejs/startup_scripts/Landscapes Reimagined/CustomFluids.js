12042514904f6521467522cbd02ca28c2662e2d0
M0nkeyPr0grammer <randoriumknights@gmail.com>
2023-11-17 16:59:24 -0600

    Update Coloring

--- kubejs/startup_scripts/Landscapes Reimagined/CustomFluids.js
+++ kubejs/startup_scripts/Landscapes Reimagined/CustomFluids.js
@@ -1,9 +1,9 @@
 StartupEvents.registry('fluid',event => {
     event.create('kubejs:liquid_dragons_breath')
-        .color(0xdb9bc3)
+        .color(0xc34781)
         .displayName('Liquid Dragons Breath')
-        .bucketColor(0xdb9bc3)
-        .thickTexture(0xdb9bc3)
+        .bucketColor(0xc34781)
+        .thickTexture(0xc34781)
         .viscosity(6000) // Similar to lava
         .temperature(1300) // Similar to lava
         .bucketItem; // Specify the bucket item here
