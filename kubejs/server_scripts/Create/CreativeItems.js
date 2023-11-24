ce22e4597d0d528d7f8c27f8acddb030b092ac65
M0nkeyPr0grammer <randoriumknights@gmail.com>
2023-11-24 12:03:05 -0600

    Created Recipes for remaining creative items

--- kubejs/server_scripts/Create/CreativeItems.js
+++ kubejs/server_scripts/Create/CreativeItems.js
@@ -1,5 +1,80 @@
 ServerEvents.recipes(event => {
 
+// Create Worldshaper
+event.custom({
+    type: "create:mechanical_crafting",
+    pattern: [
+        '   AAAAAA',
+        ' AABCDDDA',
+        'AAEFGAAAA',
+        ' AABA    ',
+        '  AA     ',
+        '  A      '
+    ],
+    key: {
+        A: Ingredient.of('minecraft:dragon_breath').toJson(),
+        B: Ingredient.of('create:creative_blaze_cake').toJson(),
+        C: Ingredient.of('advancednetherite:netherite_diamond_shovel').toJson(),
+        D: Ingredient.of('minecraft:nether_star').toJson(),
+        E: Ingredient.of('createaddition:creative_energy').toJson(),
+        F: Ingredient.of('create:creative_crate').toJson(),
+        G: Ingredient.of('deeperdarker:warden_shovel').toJson()
+    },
+    result: Ingredient.of('create:handheld_worldshaper').toJson(),
+    acceptMirrored: false
+}).id('kubejs:create/creative_worldshaper');
+
+// Create Creative Crate
+event.custom({
+    type: "create:mechanical_crafting",
+    pattern: [
+        'AAAAAAA',
+        'AABCBAA',
+        'ADEFEDA',
+        'ACGHGCA',
+        'ADEIEDA',
+        'AABCBAA',
+        'AAAAAAA'
+    ],
+    key: {
+        A: Ingredient.of('minecraft:dragon_breath').toJson(),
+        B: Ingredient.of('reinfshulker:netherite_shulker_box').toJson(),
+        C: Ingredient.of('minecraft:nether_star').toJson(),
+        D: Ingredient.of('reinfchest:netherite_chest').toJson(),
+        E: Ingredient.of('reinfbarrel:netherite_barrel').toJson(),
+        F: Ingredient.of('create:creative_fluid_tank').toJson(),
+        G: Ingredient.of('create:creative_blaze_cake').toJson(),
+        H: Ingredient.of('createaddition:creative_energy').toJson(),
+        I: Ingredient.of('create:creative_motor').toJson()
+    },
+    result: Ingredient.of('create:creative_crate').toJson(),
+    acceptMirrored: false
+}).id('kubejs:create/creative_crate');
+
+// Create Creative Generator
+event.custom({
+    type: "create:mechanical_crafting",
+    pattern: [
+        'AAAAAA',
+        'ABCCBA',
+        'ADDDDA',
+        'AEFFEA',
+        'AGGGGA',
+        'AAAAAA'
+    ],
+    key: {
+        A: Ingredient.of('minecraft:dragon_breath').toJson(),
+        B: Ingredient.of('createaddition:tesla_coil').toJson(),
+        C: Ingredient.of('minecraft:nether_star').toJson(),
+        D: Ingredient.of('createaddition:alternator').toJson(),
+        E: Ingredient.of('deeperdarker:heart_of_the_deep').toJson(),
+        F: Ingredient.of('create:creative_fluid_tank').toJson(),
+        G: Ingredient.of('createaddition:modular_accumulator').toJson()
+    },
+    result: Ingredient.of('createaddition:creative_energy').toJson(),
+    acceptMirrored: false
+}).id('kubejs:create/creative_generator');
+
 // Create Creative Motor
 event.custom({
     type: "create:mechanical_crafting",
